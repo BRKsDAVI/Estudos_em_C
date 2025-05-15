f6bdb8eb0ac1d8de9dc29858e3a65699600919d7
DAVI FIGUEIREDO DE OLIVEIRA JORGE <brksdavi123456789@gmail.com>
2025-05-15 15:50:29 -0300

    adicionando atividade de matriz
    update: adicionando atividade de matriz da atividade de programação

--- matriz/main.c
+++ matriz/main.c
@@ -6,11 +6,30 @@ int main() {
 
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 2; j++) {
-            printf("Digite o valor da linha %d e coluna %d: ", i, j);
+            printf("Digite o valor da linha %d e coluna %d: ", i+1, j+1);
             scanf("%d", &matriz[i][j]);
         }
     }
 
+    printf("\n_______________Matriz_______________\n");
+    printf("\n");
+    
+    for(i = 0; i < 5; i++) {
+        for(j = 0; j < 2; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\n_______________Vetor_______________\n");
+    printf("\n");
+
+    for(i = 0; i < 5; i++) {
+        for(j = 0; j < 2; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        
+    }
 
     return 0;
 }
